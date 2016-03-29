e1c0209c3f3015caa96dabc1ac3a08406fa21082
Marco L <mloiodice13@gmail.com>
2016-03-29 14:30:27 +0200

    Generate Key

--- app/src/main/java/fr/loiodice_marco/fingerprintdemo/MainActivity.java
+++ app/src/main/java/fr/loiodice_marco/fingerprintdemo/MainActivity.java
@@ -1,6 +1,9 @@
 package fr.loiodice_marco.fingerprintdemo;
 
+import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
+import android.os.Build;
+import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.hardware.fingerprint.FingerprintManagerCompat;
@@ -9,15 +12,19 @@ import android.os.Bundle;
 import android.app.KeyguardManager;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyStore;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.cert.CertificateException;
 
 import javax.crypto.KeyGenerator;
 
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    private static final String KEY_NAME = "example_key";
     private FingerprintManagerCompat fingerprintManagerCompat;
     private KeyguardManager keyguardManager;
     private KeyStore keyStore;
@@ -49,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @TargetApi(Build.VERSION_CODES.M)
     protected void generateKey(){
         try {
             keyStore = KeyStore.getInstance("AndroidKeyStore");
@@ -60,6 +68,19 @@ public class MainActivity extends AppCompatActivity {
             keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
         } catch (NoSuchAlgorithmException | NoSuchProviderException e){
             throw new RuntimeException("Failed to get KeyGenerator instance", e);
+        }
+
+        try {
+            keyStore.load(null);
+            keyGenerator.init(new KeyGenParameterSpec.Builder(KEY_NAME, KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC).setUserAuthenticationRequired(true).setEncryptionPaddings(
+                            KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+
+            keyGenerator.generateKey();
+        }
+        catch (NoSuchAlgorithmException | InvalidAlgorithmParameterException | CertificateException | IOException e){
+            throw new RuntimeException(e);
         }
     }
 }
