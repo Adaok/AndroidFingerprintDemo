b7fdfa37fb2bf01344136170b6ca1aad4ed80c59
Marco L <mloiodice13@gmail.com>
2016-03-29 14:11:58 +0200

    Access Android KeyStore and KeyGenerator

--- app/src/main/java/fr/loiodice_marco/fingerprintdemo/MainActivity.java
+++ app/src/main/java/fr/loiodice_marco/fingerprintdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package fr.loiodice_marco.fingerprintdemo;
 
 import android.content.pm.PackageManager;
+import android.security.keystore.KeyProperties;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.hardware.fingerprint.FingerprintManagerCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -8,11 +9,19 @@ import android.os.Bundle;
 import android.app.KeyguardManager;
 import android.widget.Toast;
 
+import java.security.KeyStore;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
+import javax.crypto.KeyGenerator;
+
 
 public class MainActivity extends AppCompatActivity {
     
     private FingerprintManagerCompat fingerprintManagerCompat;
     private KeyguardManager keyguardManager;
+    private KeyStore keyStore;
+    private KeyGenerator keyGenerator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +46,20 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Register at least one fingerprint in Settings", Toast.LENGTH_LONG).show();
             return;
         }
-        
-        
+
+    }
+
+    protected void generateKey(){
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+        } catch (NoSuchAlgorithmException | NoSuchProviderException e){
+            throw new RuntimeException("Failed to get KeyGenerator instance", e);
+        }
     }
 }
